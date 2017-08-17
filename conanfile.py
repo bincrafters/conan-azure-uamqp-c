8c33fb46757de4843c7d71b85a93f482bc32fc96
Jerry W <jerrywiltse@gmail.com>
2017-08-17 12:46:32 -0400

    almost working

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake, os
+from conans import ConanFile, CMake, os, tools
 
 
 class AzureUAMQPCConan(ConanFile):
@@ -12,13 +12,28 @@ class AzureUAMQPCConan(ConanFile):
     description = "AMQP library for C"
     license = "https://github.com/Azure/azure-uamqp-c/blob/master/LICENSE"
     lib_short_name = "azure-uamqp-c"
+    build_requires = "Azure-CTest/1.1.0@bincrafters/testing", \
+                        "Azure-C-Testrunnerswitcher/1.1.0@bincrafters/testing", \
+                        "uMock-C/1.1.0@bincrafters/testing"
+    requires = "Azure-C-Shared-Utility/1.0.41@bincrafters/testing"
     
     def source(self):
         self.run("git clone --depth=1 --branch={0} {1}.git"
                 .format(self.git_tag, self.source_url)) 
 
     def build(self):
+        tools.replace_in_file("add_subdirectory(add_subdirectory(deps/azure-c-testrunnerswitcher))","")
+        tools.replace_in_file("add_subdirectory(deps/azure-ctest)","")
+        tools.replace_in_file("add_subdirectory(deps/umock-c)","")    
+        tools.replace_in_file("add_subdirectory(deps/azure-c-shared-utility)","")    
+        tools.replace_in_file("include(\"dependencies.cmake\")","")    
+                
+        tools.replace_in_file(
+            "target_link_libraries(uamqp aziotsharedutil)",
+            "target_link_libraries(uamqp $CONAN_LIBS)") 
+           
         cmake = CMake(self)
+        cmake.args = ["use_installed_dependencies"]
         cmake.configure(source_dir=self.lib_short_name, build_dir="./")
         cmake.build()
         
