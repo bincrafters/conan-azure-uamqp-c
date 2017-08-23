56cbf4ff9a5f4a30a2336537c546939d717cd430
Jerry W <jerrywiltse@gmail.com>
2017-08-22 22:03:19 -0400

    replaced all mqtt with amqp

---
+++ test_package/amqp_client_sample.h
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+
+#ifndef AMQP_CLIENT_SAMPLE_H
+#define AMQP_CLIENT_SAMPLE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    void amqp_client_sample_run(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // AMQP_CLIENT_SAMPLE_H
---
+++ test_package/conanfile.py
@@ -0,0 +1,26 @@
+from conans import ConanFile, CMake
+import os
+import platform
+
+class AzureuamqpcTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    user = os.getenv("CONAN_USERNAME", "bincrafters")
+    channel = os.getenv("CONAN_CHANNEL", "testing")
+    requires = "Azure-uAMQP-C/1.0.41@%s/%s" % (user, channel)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        cmake.build()
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
+
+    def test(self):
+        os.chdir("bin")
+        app_name = "amqp_client_sample"
+        if platform.system() == "Windows":
+            app_name += ".exe"
+        assert(os.path.isfile(app_name))
