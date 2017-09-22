67a6f85175875ffc0f117b51ea9e28cfc05d5533
Jerry W <jerrywiltse@gmail.com>
2017-09-21 23:57:12 -0400

    standardize test_package and build.py

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,31 +1,24 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 import os
-import platform
-import subprocess
 
 
-class AzureuamqpcTestConan(ConanFile):
+class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    user = os.getenv("CONAN_USERNAME", "bincrafters")
-    channel = os.getenv("CONAN_CHANNEL", "testing")
-    requires = "Azure-uAMQP-C/1.0.41@%s/%s" % (user, channel)
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        cmake.configure()
         cmake.build()
-
+        
+    def imports(self):
+        self.copy("*", dst="bin", src="lib")
+        
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")
         self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy("*.so*", dst="bin", src="lib")
+        self.copy("*.so*", dst="bin", src="lib")        
         self.copy("*.cmake", dst="res", src="res")
-
+        
     def test(self):
-        app_name = "local_client_sample"
-        if platform.system() == "Windows":
-            app_name += ".exe"
-        assert(os.path.isfile(os.path.join("res", "uamqpConfig.cmake")))
-        assert(os.path.isfile(os.path.join("bin", app_name)))
-        subprocess.check_call(os.path.join("bin", app_name))
+        self.run(os.path.join("bin","test_package"))
