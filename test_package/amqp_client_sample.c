6a2aac77612c5abc987d32171168d525e71945a2
Uilian Ries <uilianries@gmail.com>
2017-10-26 13:40:57 -0200

    Change cmake resource path
    - To use C shared utility, was necessary change some paths
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/amqp_client_sample.c
+++ test_package/amqp_client_sample.c
@@ -8,9 +8,7 @@
 #include "azure_c_shared_utility/platform.h"
 #include "azure_c_shared_utility/socketio.h"
 #include "azure_c_shared_utility/tickcounter.h"
-#include "azure_uamqp_c/message_sender.h"
-#include "azure_uamqp_c/message.h"
-#include "azure_uamqp_c/messaging.h"
+#include "azure_uamqp_c/uamqp.h"
 
 #if _WIN32
 #include "windows.h"
@@ -98,7 +96,7 @@ int main(int argc, char** argv)
             {
                 for (i = 0; i < msg_count; i++)
                 {
-                    (void)messagesender_send(message_sender, message, on_message_send_complete, message);
+                    (void)messagesender_send_async(message_sender, message, on_message_send_complete, message, 0);
                 }
 
                 message_destroy(message);
